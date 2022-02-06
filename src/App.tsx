50ea1f75bc5d952aaf3775ccad5b7e380c41d232
Justine Kizhakkinedath <justine@kizhak.com>
2022-02-06 15:44:25 +0530

    Add logic for request chaining

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import "./styles.css";
 import { useResource } from "./useResource";
 import { useState } from "react";
+import { ChainedRequestConfigType } from "./interfaces";
 
 export default function App() {
   const [pageNumber, setPageNumber] = useState(1);
-  const getConfig = (pageNum = pageNumber) => ({
-    url: `https://jsonplaceholder.typicode.com/todos/${pageNum}`
-  });
+  const getConfig = (pageNum = pageNumber): ChainedRequestConfigType[] => [
+    {
+      baseConfig: {
+        url: `https://jsonplaceholder.typicode.com/todos/1`
+      },
+      onFinal: (acc, next) => {
+        // console.log("finally 1: ", acc);
+      }
+    },
+    {
+      baseConfig: {
+        url: `https://jsonplaceholder.typicode.com/todos/2`
+      },
+      onFinal: (acc, next) => {
+        // console.log("finally 2: ", acc);
+      }
+    }
+  ];
 
   const { data, Container, refetch } = useResource(getConfig(), "todoDetails");
 
