3b702b7c72659f94b7985912ac2c5305bd7a8c09
Justine Kizhakkinedath <justine@kizhak.com>
2022-02-07 21:26:34 +0530

    initial commit

--- src/lib/resourceContext.tsx
+++ src/lib/resourceContext.tsx
@@ -2,6 +2,10 @@ import { createContext } from "react";
 import { GlobalResourceContextType } from "./interfaces";
 
 export const GlobalResourceContext = createContext<GlobalResourceContextType>({
-  dispatch: () => {},
-  selector: () => {}
+  dispatch: () => {
+    console.info("Please add a global resource context provider.");
+  },
+  selector: () => {
+    console.info("Please add a global resource context provider.");
+  }
 });
