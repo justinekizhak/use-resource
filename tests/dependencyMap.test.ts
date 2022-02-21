a1d90a394d9e8a2fcf1109944907a949dff3fd4f
Justine Kizhakkinedath <justine@kizhak.com>
2022-02-21 23:35:22 +0530

    Add unit tests

---
+++ tests/dependencyMap.test.ts
@@ -0,0 +1,39 @@
+import {
+  createDependencyMap,
+  validateRequestChain
+} from "../src/lib/requestChain/dependencyMap";
+import {
+  getRequestChain,
+  getInvalidRequestChain,
+  getCyclicRequestChain
+} from "./helpers/requestChain";
+
+describe("Testing dependency graph", () => {
+  test("dependency start: POSITIVE", () => {
+    const requestChain = getRequestChain();
+    const { dependencyMap, start } = createDependencyMap(requestChain);
+    const expectedDependencyMap = { "1ReqId": null, "2ReqId": ["1ReqId"] };
+    expect(start).toBe("1ReqId");
+    expect(dependencyMap).toStrictEqual(expectedDependencyMap);
+  });
+
+  test("Check for Invalid dependency: POSITIVE", () => {
+    const requestChain = getRequestChain();
+    const isValid = validateRequestChain(requestChain);
+    expect(isValid).toBe(true);
+  });
+
+  test("Check for Invalid dependency: NEGATIVE", () => {
+    const requestChain = getInvalidRequestChain();
+    expect(() => {
+      validateRequestChain(requestChain);
+    }).toThrow(/Invalid deps: /);
+  });
+
+  test.skip("Check for cyclic dependency: NEGATIVE", () => {
+    const requestChain = getCyclicRequestChain();
+    expect(() => {
+      validateRequestChain(requestChain);
+    }).toThrow(/Dependency cycle found: /);
+  });
+});
