ceb07b3a903a1ced3e0407aa92edb777fd1144e7
Justine Kizhakkinedath <justine@kizhak.com>
2022-04-08 16:40:50 +0530

    refactor: Move the page into components folder

---
+++ src/components/homepage/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import clsx from "clsx";
+import Layout from "@theme/Layout";
+// import Link from "@docusaurus/Link";
+// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+// import styles from "./index.module.css";
+import "./windi.css";
+
+export default function Home() {
+  // const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`useResource hook`}
+      description="Easiest way to do API calls"
+    >
+      <main>Use Resource Hook</main>
+    </Layout>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
+import HomepageComponent from "../components/homepage";
 
-export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`useResource hook`}
-      description="Easiest way to do API calls"
-    >
-      <main>Use Resource Hook</main>
-    </Layout>
-  );
+export default function HomePage() {
+  return <HomepageComponent />;
 }
